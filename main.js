50b976d245e1ff3fbb7e5d95ba6bfd18ca5cc765
bautipietra <bautistapietraroia@gmail.com>
2021-09-19 12:40:51 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let usuario = prompt(`Hola! Ingresa tu nombre de usuario para continuar`);
+let numero1 = Number(prompt(`Ingresa el primer numero que quiera sumar`));
+let numero2 = Number(prompt(`Ingresa otro numero`));
+
+let resultado = numero1 + numero2;
+
+alert(`Hola ${usuario}! La suma da como resultado ${resultado}`);
